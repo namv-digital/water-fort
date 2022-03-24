8c34060c1b71bd4f7977d40261cc0b4486b574d0
namv-digital <namv.digital@gmail.com>
2022-03-23 22:28:18 -0700

    hanle from email

--- pages/api/contact.ts
+++ pages/api/contact.ts
@@ -22,6 +22,10 @@ export default async function handler(
     const draft = nylas.drafts.build({
       subject: 'Contact Request',
       to: [{ name: 'My Nylas Friend', email: process.env.MAIL_TO }],
+      from: [{
+        name: process.env.MAIL_FROM_NAME,
+        email: process.env.MAIL_FROM_ADDRESS
+      }],
       body: `Contact form from ${req.body.email}`,
     })
 
