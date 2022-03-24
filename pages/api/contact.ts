5c89df4a118aac81c5aabf9688cf9397071c1376
namv-digital <namv.digital@gmail.com>
2022-03-23 20:42:07 -0700

    email

--- pages/api/contact.ts
+++ pages/api/contact.ts
@@ -5,7 +5,7 @@ type Data = {
   success: boolean
 }
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
@@ -21,6 +21,19 @@ export default function handler(
     secure: true,
   })
 
+  await new Promise((resolve, reject) => {
+    // verify connection configuration
+    transporter.verify(function (error: any, success: any) {
+      if (error) {
+        console.log(error)
+        reject(error)
+      } else {
+        console.log('Server is ready to take our messages')
+        resolve(success)
+      }
+    })
+  })
+
   try {
     const mailData = {
       from: '"Fred Foo 👻" <liquidity@waterfort.io>',
@@ -29,6 +42,19 @@ export default function handler(
       text: `Contact form from ${req.body.email}`, // plain text body
       html: `<b>Contact form from ${req.body.email}</b>`, // html body
     }
+
+    await new Promise((resolve, reject) => {
+      // send mail
+      transporter.sendMail(mailData, (err: any, info: any) => {
+        if (err) {
+          console.error(err)
+          reject(err)
+        } else {
+          console.log(info)
+          resolve(info)
+        }
+      })
+    })
 
     transporter.sendMail(mailData, function (err: any, info: any) {
       if (err) console.log(err)
