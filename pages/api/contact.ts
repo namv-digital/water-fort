e3114438606626688a298d8698ee18cae4328393
namv-digital <namv.digital@gmail.com>
2022-03-23 20:59:21 -0700

    rollback

--- pages/api/contact.ts
+++ pages/api/contact.ts
@@ -2,8 +2,7 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 type Data = {
-  success: boolean,
-  address: string
+  success: boolean
 }
 
 export default async function handler(
@@ -12,55 +11,53 @@ export default async function handler(
 ) {
   let nodemailer = require('nodemailer')
 
-  // const transporter = nodemailer.createTransport({
-  //   port: 465,
-  //   host: 'smtp.gmail.com',
-  //   auth: {
-  //     user: process.env.MAIL_ADDRESS,
-  //     pass: process.env.MAIL_PASSWORD,
-  //   },
-  //   secure: true,
-  // })
+  const transporter = nodemailer.createTransport({
+    port: 465,
+    host: 'smtp.gmail.com',
+    auth: {
+      user: process.env.MAIL_ADDRESS,
+      pass: process.env.MAIL_PASSWORD,
+    },
+    secure: true,
+  })
 
-  // await new Promise((resolve, reject) => {
-  //   // verify connection configuration
-  //   transporter.verify(function (error: any, success: any) {
-  //     if (error) {
-  //       console.log('verify error', error)
-  //       reject(error)
-  //     } else {
-  //       console.log('Server is ready to take our messages')
-  //       resolve(success)
-  //     }
-  //   })
-  // })
+  await new Promise((resolve, reject) => {
+    // verify connection configuration
+    transporter.verify(function (error: any, success: any) {
+      if (error) {
+        console.log('verify error', error)
+        reject(error)
+      } else {
+        console.log('Server is ready to take our messages')
+        resolve(success)
+      }
+    })
+  })
 
-  // try {
-  //   const mailData = {
-  //     from: '"Fred Foo 👻" <liquidity@waterfort.io>',
-  //     to: process.env.MAIL_TO,
-  //     subject: 'Contact request ✔', // Subject line
-  //     text: `Contact form from ${req.body.email}`, // plain text body
-  //     html: `<b>Contact form from ${req.body.email}</b>`, // html body
-  //   }
+  try {
+    const mailData = {
+      from: '"Fred Foo 👻" <liquidity@waterfort.io>',
+      to: process.env.MAIL_TO,
+      subject: 'Contact request ✔', // Subject line
+      text: `Contact form from ${req.body.email}`, // plain text body
+      html: `<b>Contact form from ${req.body.email}</b>`, // html body
+    }
 
-  //   await new Promise((resolve, reject) => {
-  //     // send mail
-  //     transporter.sendMail(mailData, (err: any, info: any) => {
-  //       if (err) {
-  //         console.error('send error', err)
-  //         reject(err)
-  //       } else {
-  //         console.log(info)
-  //         resolve(info)
-  //       }
-  //     })
-  //   })
+    await new Promise((resolve, reject) => {
+      // send mail
+      transporter.sendMail(mailData, (err: any, info: any) => {
+        if (err) {
+          console.error('send error', err)
+          reject(err)
+        } else {
+          console.log(info)
+          resolve(info)
+        }
+      })
+    })
 
-  //   res.status(200).json({ success: true })
-  // } catch (error: any) {
-  //   res.status(200).json({ success: false })
-  // }
-
-  res.status(200).json({ success: true, address: process.env.MAIL_ADDRESS || "" })
+    res.status(200).json({ success: true })
+  } catch (error: any) {
+    res.status(200).json({ success: false })
+  }
 }
